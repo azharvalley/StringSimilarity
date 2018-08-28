3e01e901140819a4e64101fcbbd1e993620cf841
Konrad Gontek <konrad.gontek@gmail.com>
2018-08-28 07:52:39 +0200

    add tests for Dice Coefficient

--- Tests/StringSimilarity.cs
+++ Tests/StringSimilarity.cs
@@ -19,10 +19,29 @@ namespace Tests
         [InlineData("test string", "another test")]
         [InlineData("another string", "another test")]
         [InlineData("another test", "another string")]
-        public void LevenstainMethodForVariousString(string source, string target)
+        public void LevenstainMethodForVariousStrings(string source, string target)
         {
             var result = LevenstainDistance.CalculateSimilarity(source, target);
             Assert.True( result < 1 && result > 0);
+        }
+
+        [Fact]
+        public void DiceCoefficientForEqualStrings()
+        {
+            var string1 = "system under test";
+            var string2 = string1;
+
+            Assert.True(DiceCoefficient.DiceCoefficientCount(string1, string2) == 1);
+        }
+
+        [Theory]
+        [InlineData("tests aaaa", "bbbb tests")]
+        public void DiceCoefficientForVariousStrings(string source, string target)
+        {
+            var result = DiceCoefficient.DiceCoefficientCount(source, target);
+      
+            var expected = (2.0 * 6) / (source.Length + target.Length);
+            Assert.True(Math.Round(result, 1) == Math.Round(expected, 1));
         }
     }
 }
